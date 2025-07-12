016efeca62195d512bc8651d01bc6be16809b4e7
aviansh771 <avinash122023@gmail.com>
2025-07-12 20:29:38 +0530

    backend

--- Azurerm_resource_group/provider.tf
+++ Azurerm_resource_group/provider.tf
@@ -1,4 +1,11 @@
 terraform {
+     backend "azurerm" {
+    resource_group_name  = "infra_rg"          # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
+    storage_account_name = "stg0007"       # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    container_name       = "cnt007"         # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+    key                  = "prod.terraform.tfstate" # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+  }
+  
 required_providers {
     azurerm = {
         source = "hashicorp/azurerm"
